0e9cf1c49aa86e81f746a26d9742187c30e2c383
Binh314 <binhphamc2002@gmail.com>
2024-03-06 15:26:53 -0500

    cleaned up code a little bit

--- safety_controller/safety_controller.py
+++ safety_controller/safety_controller.py
@@ -8,46 +8,54 @@ from visualization_msgs.msg import Marker
 
 from safety_controller.visualization_tools import VisualizationTools
 
-
 class SafetyController(Node):
 
     def __init__(self):
         super().__init__("safety_controller")
         # Declare parameters to make them available for use
         self.declare_parameter("scan_topic", "/scan")
-        self.declare_parameter("drive_topic", "/drive")
-        self.VELOCITY = 4.0
+        self.declare_parameter("drive_topic", "/vesc/high_level/output")
+        self.declare_parameter("stop_topic", "/vesc/low_level/input/safety")
+        self.speed = 1.0
 
         # Fetch constants from the ROS parameter server
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
+        self.STOP_TOPIC = self.get_parameter('stop_topic').get_parameter_value().string_value
         
         self.WALL_TOPIC = "/wall"
 
         self.drive_msg = None
+            
+        self.DRIVE_TOPIC = "/vesc/high_level/ackermann_cmd"
+        self.get_logger().info(self.DRIVE_TOPIC)
 
-
-	    # Initialize your publishers and subscribers here
-        self.publisher_ = self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 10)
+        # Initialize your publishers and subscribers here
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, self.STOP_TOPIC, 10)
         self.subscription = self.create_subscription(
-            LaserScan,
-            self.SCAN_TOPIC,
-            self.lidar_callback,
-            10)
-        self.subscription = self.create_subscription(
-            AckermannDriveStamped,
-            self.DRIVE_TOPIC,
-            self.drive_callback,
-            10)
+           LaserScan,
+           self.SCAN_TOPIC,
+           self.lidar_callback,
+           10)
+        self.subscription2 = self.create_subscription(
+           AckermannDriveStamped,
+           self.DRIVE_TOPIC,
+           self.drive_callback,
+           10)
         
 
         self.CAR_LENGTH = 0.3 # meters
-        self.CAR_WIDTH = 0.32 # meters (not sure if value is correct, just guessing)
-
-        # UNCOMMENT LINE BELOW FOR TESTING
-        # self.drive_forward()
+        self.CAR_WIDTH = 0.32 # meters (includes buffer)
+        self.DIST_TO_BUMPER = 0.12
+        self.get_logger().info("SAFETY CONTROLLER STARTED!")
 
     # Write your callback functions here 
+
+    # intercept latest drive command
+    def drive_callback(self, drive):
+        self.drive_msg = drive
+
+
     def drive_forward(self):
         self.get_logger().info('Drive Forward')
         drive_msg = AckermannDriveStamped()
@@ -71,14 +79,27 @@ class SafetyController(Node):
 
 
     def lidar_callback(self, scan):
+        # Fetch constants from the ROS parameter server
+        self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
+        self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
+
         # self.get_logger().info('Received scan!')
         MAX_STEER = 0.34
-        FUTURE_DIST = 0.03
         steering_angle = 0.0
 
-        if self.drive_msg:
-            FUTURE_DIST += 0.01 * self.drive_msg.drive.speed
+        if self.drive_msg is not None:
+            #if self.drive_msg.drive.speed > 0.0:
+            #    self.get_logger().info('"%s"' % self.drive_msg.drive.speed)
+            # if self.drive_msg.drive.speed > self.SPEED:
+            self.speed = self.drive_msg.drive.speed
             steering_angle = self.drive_msg.drive.steering_angle
+
+        #react_time = 0.05
+        #max_deceleration = 14.0 # m/s^2
+        #stop_dist = 2.0 * self.speed / max_deceleration + react_time * self.speed
+        
+        # found via experimenting
+        stop_dist = (0.41 * self.speed + 0.2) ** 2
 
         # polar lidar coordinates to cartesian coordinates
         ranges = np.array(scan.ranges)
@@ -90,26 +111,62 @@ class SafetyController(Node):
         x = ranges * np.cos(angles)
         y = ranges * np.sin(angles)
 
-        # create rectangle in front of car
-        low_y = - self.CAR_WIDTH / 2 + 0.02 * min(0, steering_angle / MAX_STEER)
-        high_y = self.CAR_WIDTH / 2 + 0.02 * max(0, steering_angle / MAX_STEER)
-        low_x = 0
-        high_x = 4.0 * FUTURE_DIST
+        # create parallelogram in front of car
+        low_y = - self.CAR_WIDTH / 2 + (x) * np.arctan(steering_angle)
+        high_y = self.CAR_WIDTH / 2 + (x) * np.arctan(steering_angle)
+        low_x = self.DIST_TO_BUMPER
+        high_x = self.DIST_TO_BUMPER + stop_dist
 
         # check if any points in rectangle
-        within_front = np.where(np.logical_and(y >= low_y, y <= high_y))
+        within_front = np.where(np.logical_and(np.greater_equal(y, low_y), np.less_equal(y, high_y)))
         x_within = x[within_front]
         close_front = np.logical_and(x_within >= low_x, x_within <= high_x)
+
+        
+        # can't create circle if steering angle is exactly 0
+        if np.any(close_front):
+            self.stop()
+            return
+
+        # IF HERE, THERE IS A STEERING ANGLE
+
+        # create circular section in front of car
+        turn_radius = np.abs(self.CAR_LENGTH / np.sin(steering_angle))
+        turn_sign = np.sign(steering_angle) # -1 for right, 1 for left turn
+        inner_radius = turn_radius - self.CAR_WIDTH / 2.0
+        outer_radius = turn_radius - self.CAR_WIDTH / 2.0
+
+        inner_circle = turn_sign * inner_radius - turn_sign * np.sqrt(inner_radius**2 - x**2)
+        outer_circle = turn_sign * outer_radius - turn_sign * np.sqrt(outer_radius**2 - x**2)
+
+        # end of the circular section
+        dist_line = - turn_sign * np.tan(np.pi / 2 - stop_dist / turn_radius) * x + turn_sign * turn_radius
+        
+        if turn_sign == 1: # LEFT
+            upper_side = inner_circle
+            lower_side = outer_circle
+            within_turn = np.where(np.logical_and(lower_side <= y, y <= upper_side))
+            y_within = y[within_turn]
+            dist_line = dist_line[within_turn]
+            close_front = y_within <= dist_line
+        if turn_sign == -1: # RIGHT
+            upper_side = outer_circle
+            lower_side = inner_circle
+            within_turn = np.where(np.logical_and(lower_side <= y, y<= upper_side))
+            y_within = y[within_turn]
+            dist_line = dist_line[within_turn]
+            close_front = y_within >= dist_line
+
+        if np.any(close_front):
+            self.stop()
+        
+        
 
         if np.any(close_front):
             # UNCOMMENT WHILE LOOP BELOW FOR TESTING WITH WALL FOLLOWER IN SIM
             # while True: # blocking code >:)
             #     self.stop()
             self.stop()
-
-
-    def drive_callback(self, drive):
-        self.drive_msg = drive
 
 def main():
 
